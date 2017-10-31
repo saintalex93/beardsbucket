128eca9d5355374fa588cf6e8665f5c621405c22
Diego Rodrigues <diego.rodrigues.joaquim@gmail.com>
2017-10-30 23:33:59 -0200

    cadastro beards bucket

--- sistema/src/insertCadastroUser.php
+++ sistema/src/insertCadastroUser.php
@@ -18,23 +18,32 @@ $saldoConta = $_POST['saldoContaCadastro'];
 $conecta = mysqli_connect('localhost', 'bucket', '123', 'BUCKET');
 mysqli_set_charset($conecta, "utf8");
 
-$cSQL = "insert into USUARIO(USR_COD,USR_SENHA,USR_LOGIN,USR_NOME,USR_EMAIL,USR_PERMISSAO,USR_STATUS)values(0,'$senha','$login','$nome','$email',1,1)";
+$oSQL = "insert into USUARIO(USR_SENHA,USR_LOGIN,USR_NOME,USR_EMAIL,USR_PERMISSAO,USR_STATUS)values('$senha','$login','$nome','$email',1,1)";
 
 echo $oSQL . '</br>';
 
-$Dados = mysqli_query($conecta,$oSQL);
+$codUsuario = mysqli_insert_id($conecta);
+$dados= mysqli_query($conecta, $oSQL);
+echo $codUsuario . '</br>';
+
+
 
 $oSQL = "INSERT INTO EMPRESA (EMP_COD,EMP_NOME_EMPRESA,EMP_CNPJ,EMP_STATUS)VALUES(0,'$nomeEmpresa', '$cnpjEmpresa',2)";
 
 echo $oSQL . '</br>';
+$dados= mysqli_query($conecta, $oSQL);
+$codEmpresa = mysqli_insert_id($conecta);
+echo $dados . '</br>';
 
-$Dados = mysqli_query($conecta,$oSQL);
 
-$oSQL = "INSERT INTO CONTA (CNT_COD,CNT_NOME,CNT_BANCO,CNT_AGNC,CNT_NMCONTA,CNT_TIPO,CNT_STATUS,CNT_SALDOINICIAL,COD_EMPR) VALUES(0, '$nomeConta', '$bancoConta', '$bancoConta', '$numeroConta', '$tipoConta',$saldoConta,1)";
 
-echo $oSQL;
 
-$Dados = mysqli_query($conecta,$oSQL);
+$oSQL = "INSERT INTO CONTA (CNT_COD,CNT_NOME,CNT_BANCO,CNT_AGNC,CNT_NMCONTA,CNT_TIPO,CNT_STATUS,CNT_SALDOINICIAL,COD_EMPR) VALUES(0, '$nomeConta', '$bancoConta', '$bancoConta', '$numeroConta', 'cc',$saldoConta,1)";
+
+echo $oSQL . '</br>';
+$dados= mysqli_query($conecta, $oSQL);
+
+$codConta = mysqli_insert_id($conecta);
 
 mysqli_close($conecta);
 
